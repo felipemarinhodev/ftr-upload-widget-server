14a6f1ef7047e0deb8cac2d9648b3d055a090b7a
Felipe Marinho <github@felipemarinho.dev>
2025-02-26 07:41:49 -0800

    add environment variable validations for PostgreSQL in env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,11 +1,16 @@
 import z from "zod"
 
 const envSchema = z.object({
+  PORT: z.coerce.number().default(3333),
   CLOUDFLARE_ACCESS_KEY_ID: z.string(),
   CLOUDFLARE_SECRET_ACCESS_KEY: z.string(),
   CLOUDFLARE_BUCKET: z.string(),
   CLOUDFLARE_ACCOUNT_ID: z.string(),
   CLOUDFLARE_PUBLIC_URL: z.string().url(),
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_DB: z.string(),
+  POSTGRES_PORT: z.coerce.number(),
 })
 
 export const env = envSchema.parse(process.env)
