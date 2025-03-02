3e75e62e96b651d8725568791ccd3bc6bfed11b8
Felipe Marinho <github@felipemarinho.dev>
2025-03-02 10:52:01 -0800

    change health check route from POST to GET

--- src/routes/health-check.ts
+++ src/routes/health-check.ts
@@ -1,7 +1,7 @@
 import type { FastifyInstance } from "fastify";
 
 export async function healthCheckRoute(app: FastifyInstance) {
-  app.post('/health', async (request, reply) => {
+  app.get('/health', async (request, reply) => {
     await reply.send({  message: "OK" })
   })
 }
