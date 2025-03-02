77197a08c77009a5351c289878472becf7bb15ba
Felipe Marinho <github@felipemarinho.dev>
2025-03-02 11:37:04 -0800

    update health check response message to include thumbs up emoji

--- src/routes/health-check.ts
+++ src/routes/health-check.ts
@@ -2,6 +2,6 @@ import type { FastifyInstance } from "fastify";
 
 export async function healthCheckRoute(app: FastifyInstance) {
   app.get('/health', async (request, reply) => {
-    await reply.send({  message: "OK" })
+    await reply.send({  message: "OK 👍🏼" })
   })
 }
